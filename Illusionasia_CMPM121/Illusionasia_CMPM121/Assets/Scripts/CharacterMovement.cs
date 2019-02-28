2c5b1adf8bc8c9fdb4e55793bc2d8c1aeccd7a18
Vienna Chan <vhchan@ucsc.edu>
2019-02-27 23:41:15 -0800

    Added dummy character, scripts
    Added a simple character controller. Will probably edit it further or change the scene a bit

---
+++ Illusionasia_CMPM121/Illusionasia_CMPM121/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    /*
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    */
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } 
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+        /*
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Debug.Log(moveHorizontal);
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb.AddForce(movement * speed);
+        */
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
