4c7abd56107b4849c3fcfbd3a31de387f6f92424
Vienna Chan <vhchan@ucsc.edu>
2019-03-16 22:05:24 -0700

    Dialogue boxes and text
    Made the buttons, dialogue box, and text look better

---
+++ Illusionasia_CMPM121/Illusionasia_CMPM121/Assets/Scripts/RainScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainScript : MonoBehaviour
+{
+    public ParticleSystem rain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var particles = rain.GetComponent<ParticleSystem>();
+        particles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
